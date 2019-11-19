38361dfb38e8d669141194e4eaaede225b9190d5
Ron Whyrick <46409044+ronniewhyrick@users.noreply.github.com>
2019-11-19 13:04:38 -0600

    Serial communication established

--- TrakkerBPfinalCode/BackpackSource/BackpackSource.ino
+++ TrakkerBPfinalCode/BackpackSource/BackpackSource.ino
@@ -1,7 +1,7 @@
 #include <SoftwareSerial.h>
 //establish two software serial ports
-SoftwareSerial GPSModule(0, 1); // RX, TX
-SoftwareSerial HC12(6, 5); // HC-12 TX Pin, HC-12 RX Pin
+SoftwareSerial GPSModule(5, 4); // RX, TX
+SoftwareSerial Trakker(9, 10); // HC-12 TX Pin, HC-12 RX Pin
 int updates;
 int failedUpdates;
 int pos;
@@ -19,42 +19,44 @@ String Hlonfinal;
 String labels[12] {"Time: ", "Status: ", "Latitude: ", "Hemisphere: ", "Longitude: ", "Hemisphere: ", "Speed: ", "Track Angle: ", "Date: "};
 //backpack unique variables
 char rx_init;
+bool done = false; // Listen check complete (0 = false, 1 = true)
+
 void setup() {
   Serial.begin(9600);
-  //initialize software serial for GPS then HC12
+  //initialize software serial for GPS then Trakker
+  Trakker.begin(9600);
   GPSModule.begin(9600);
-  HC12.begin(9600);   
   Serial.println("Setup Complete");
 }
 
-void loop() {
+void loop() {     //following code till end of loop is for testing w/o GPS only. allows data to be recieved without having a working GPS.
   Serial.flush();
-  GPSModule.listen(); //activate software serial to GPS port, ignore HC12 port
-  GPSModule.flush();
-  while (GPSModule.available() > 0) //if new GPS data is available
+  //Begin reading GPS module
+  //GPSModule.listen(); // Priority set to GPS
+  while ((GPSModule.available())) //if new GPS data is available and all other process complete
   {
+    //activate software serial to GPS port, ignore Trakker port
+    GPSModule.flush();
     GPSModule.read();
-
-  }
-  if (GPSModule.find("$GPRMC,")) {
-    String tempMsg = GPSModule.readStringUntil('\n');
-    for (int i = 0; i < tempMsg.length(); i++) {
-      if (tempMsg.substring(i, i + 1) == ",") {
-        nmea[pos] = tempMsg.substring(stringplace, i);
-        stringplace = i + 1;
-        pos++;
-      }
-      if (i == tempMsg.length() - 1) {
-        nmea[pos] = tempMsg.substring(stringplace, i);
-      }
-    }
-    updates++;
+    
+    //Begin taking in nmea coordinates
+    if (GPSModule.find("$GPRMC,")) {
+      String tempMsg = GPSModule.readStringUntil('\n');
+      for (int i = 0; i < tempMsg.length(); i++) {
+        if (tempMsg.substring(i, i + 1) == ",") {
+          nmea[pos] = tempMsg.substring(stringplace, i);
+          stringplace = i + 1;
+          pos++;
+        }
+        if (i == tempMsg.length() - 1) {
+          nmea[pos] = tempMsg.substring(stringplace, i);
+        }
+      }updates++;
     //call functions to convert nmea long and lat to readable degrees
     nmea[2] = ConvertLat();
     nmea[4] = ConvertLng();
-    Serial.print("filtered self GPS coordinates");
-    //enable HC12 Software Serial port.
-    HC12.listen();
+    Serial.print("filtered self GPS coordinates \n");
+    //enable Trakker Software Serial port.
     //convert string arrays to strings, then character arrays
     latfinal = nmea[2];
     Hlatfinal = nmea[3];
@@ -65,44 +67,31 @@ void loop() {
     lonfinal.toCharArray(tx_lon,20);
     Hlonfinal.toCharArray(tx_Hlon,20);
     //print on monitor final self GPS values with labels
-    for (int i = 2; i < 6; i++) {
-      Serial.print(labels[i]);
-      Serial.print(nmea[i]);
-      Serial.println("");
+      for (int i = 2; i < 6; i++) {
+        Serial.print(labels[i]);
+        Serial.print(nmea[i]);
+        Serial.println("");
+      }
+     //Serial.println(Trakker.isListening());
     }
-    //write to HC12: lat,Hlat,lon,Hlon
-    HC12.write(tx_lat);
-    HC12.write(",");
-    HC12.write(tx_Hlat);
-    HC12.write(",");
-    HC12.write(tx_lon);
-    HC12.write(",");
-    HC12.write(tx_Hlon);
-    HC12.write('\n');
-    //Recieve GPS sent to backpack from trackers via HC12 read
-    if (HC12.available())
+    else failedUpdates++;
+    // Reset values and jump out of GPS while loop
+    stringplace = 0;
+    pos = 0;
+    //Begin reading Trakker clips
+    Trakker.listen(); //Priority set to trakker clips
+  }
+  
+  while (Trakker.available())
   {
-   rx_init = HC12.read(); //check here store variable in character array Check buffer size and latency.
+     rx_init = Trakker.read();
+     Serial.println(Trakker.read());
+     Serial.print(rx_init);
+     GPSModule.listen(); // Priority set to GPS
   }
-  }
-  else {
+}//end of void loop()
 
-    failedUpdates++;
 
-  }
-  stringplace = 0;
-  pos = 0;
-  //following code till end of loop is for testing w/o GPS only. allows data to be recieved without having a working GPS.
-  HC12.listen();
-  Serial.print(" Made IT ");
-  if (HC12.available())
-  {
-   Serial.write(HC12.read());
-   Serial.print("Gotcha");
-   rx_init = HC12.read();
-   Serial.print(rx_init);
-  }
-}
 //everything below are just functions to convert nmea data to readable degrees and hemispherical information
 String ConvertLat() {
   String posneg = "";
@@ -136,7 +125,6 @@ String ConvertLng() {
   if (nmea[5] == "W") {
     posneg = "-";
   }
-
   String lngfirst;
   float lngsecond;
   for (int i = 0; i < nmea[4].length(); i++) {
